a1d73632e4ea29c141a9308276a9b027b531f429
GitElena <elena.mastria@unical.it>
2026-04-08 14:05:34 +0200

    update admin.py

--- titulus_utility/admin.py
+++ titulus_utility/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from titulus_utility.models import CredentialWSProtocollo, ConfigurationWSProtocollo
+from titulus_utility.models import CredentialWSProtocollo, ConfigurationWSProtocollo, VoceIndice
 
 admin.site.register(ConfigurationWSProtocollo)
 admin.site.register(CredentialWSProtocollo)
+admin.site.register(VoceIndice)
